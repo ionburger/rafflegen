266ab09e3464d2c0d243b7eb578ef98f170ec99b
ionburger <ionburgger@gmail.com>
2022-12-10 19:43:42 -0700

    eeeee

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
-logger.debug("test")
+import tkinter as tk
+from tkinter import filedialog
+
+# Create the main window
+window = tk.Tk()
+window.title("File Selection")
+
+# Create a function that will be called when the user clicks the button
+def select_file():
+    # Use the filedialog module to create a file selection dialog box
+    selected_file = filedialog.askopenfilename()
+    # Print the path to the selected file
+    print(selected_file)
+
+# Create a button that will open the file selection dialog when clicked
+file_button = tk.Button(window, text="Select File", command=select_file)
+file_button.pack()
+
+# Start the main event loop to display the window and wait for user input
+window.mainloop()
